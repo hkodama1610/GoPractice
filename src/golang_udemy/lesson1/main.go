970f29a0df7a0465973506cf4f4b6871dfca4730
Hiroto Kodama <h.kodama1610@gmail.com>
2023-08-07 21:18:07 +0900

    ファイル追加

---
+++ src/golang_udemy/lesson1/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// 配列
+func main() {
+	var arr1 [3]int
+	fmt.Println(arr1)
+	fmt.Printf("%T\n", arr1)
+
+	var arr2 [3]string = [3]string{"A", "B"}
+	fmt.Println(arr2)
+
+	arr3 := [3]int{1, 2, 3}
+	fmt.Println(arr3)
+
+	arr4 := [...]string{"C"}
+
+}
